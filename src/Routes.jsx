6ef2dbb0e0e92adbf2c02549d73c7f0f21b06ac2
pedroluizfo <pedroluizfo@gmail.com>
2023-12-04 18:21:55 -0300

    estrutura inicial front

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "pages/Home";
 import NotFound from "pages/NotFound";
 const Landingpage = React.lazy(() => import("pages/Landingpage"));
 const Property1VariantTwo = React.lazy(
@@ -11,13 +10,9 @@ const ProjectRoutes = () => {
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landingpage />} />
           <Route path="*" element={<NotFound />} />
-          <Route
-            path="/property1varianttwo"
-            element={<Property1VariantTwo />}
-          />
-          <Route path="/landingpage" element={<Landingpage />} />
+       
         </Routes>
       </Router>
     </React.Suspense>
--- src/pages/Landingpage/index.jsx
+++ src/pages/Landingpage/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import  Property1VariantTwoPage from "../Property1VariantTwo/index"
 import { Img, Text } from "components";
 
 const LandingpagePage = () => {
   return (
     <>
+    <Property1VariantTwoPage/>
       <div className="bg-gray-300 flex sm:flex-col md:flex-col flex-row font-pressstartp sm:gap-10 md:gap-10 gap-[325px] items-end justify-between mx-auto w-auto sm:w-full md:w-full">
+        
         <Img
           className="h-[1080px] md:h-auto object-cover w-[235px] sm:w-full"
           src="images/img_frame71.png"
